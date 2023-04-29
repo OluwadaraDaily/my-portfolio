baf61153ee62f45a586c212a0c77f6489beb8838
Oluwadara Oloye <daraoloye99@gmail.com>
2023-04-29 20:13:44 +0100

    Implement header component

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -1,5 +1,8 @@
 <script setup>
-import GitHub from '../icons/GitHub.vue';
+import GitHub from '../icons/GitHub.vue'
+import LinkedIn from '../icons/LinkedIn.vue'
+import Twitter from '../icons/Twitter.vue'
+import FrontEndMentor from '../icons/FrontEndMentor.vue'
 </script>
 
 <template>
@@ -8,11 +11,22 @@ import GitHub from '../icons/GitHub.vue';
       <p class="portfolio__header__name__title">oluwadara oloye</p>
     </div>
     <div class="portfolio__header__links">
-
+      <a href="https://github.com/OluwadaraDaily" target="__blank">
+        <GitHub />
+      </a>
+      <a href="https://www.linkedin.com/in/oluwadara-oloye" target="__blank">
+        <LinkedIn />
+      </a>
+      <a href="https://twitter.com/OluwadaraDaily" target="__blank">
+        <Twitter />
+      </a>
+      <a href="https://www.frontendmentor.io/profile/OluwadaraDaily" target="__blank">
+        <FrontEndMentor />
+      </a>
     </div>
   </header>
 </template>
 
 <style lang="scss" scoped>
-  @import './Header.scss'
+@import './Header.scss';
 </style>
