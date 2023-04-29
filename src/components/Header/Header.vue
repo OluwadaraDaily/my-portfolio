edeb95f8ba6ec6e59ad23e569a8b2469519f47a2
Oluwadara Oloye <daraoloye99@gmail.com>
2023-04-29 19:57:51 +0100

    Initial commit

---
+++ src/components/Header/Header.vue
@@ -0,0 +1,18 @@
+<script setup>
+import GitHub from '../icons/GitHub.vue';
+</script>
+
+<template>
+  <header>
+    <div class="portfolio__header__name">
+      <p class="portfolio__header__name__title">oluwadara oloye</p>
+    </div>
+    <div class="portfolio__header__links">
+
+    </div>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+  @import './Header.scss'
+</style>
