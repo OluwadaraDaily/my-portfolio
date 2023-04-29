0b8047bf60648ebe273432fb9ea41e3c5cfba977
Oluwadara Oloye <daraoloye99@gmail.com>
2023-04-29 21:04:44 +0100

    Implement header component completion

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -10,19 +10,21 @@ import FrontEndMentor from '../icons/FrontEndMentor.vue'
     <div class="portfolio__header__name">
       <p class="portfolio__header__name__title">oluwadara oloye</p>
     </div>
-    <div class="portfolio__header__links">
-      <a href="https://github.com/OluwadaraDaily" target="__blank">
-        <GitHub />
-      </a>
-      <a href="https://www.linkedin.com/in/oluwadara-oloye" target="__blank">
-        <LinkedIn />
-      </a>
-      <a href="https://twitter.com/OluwadaraDaily" target="__blank">
-        <Twitter />
-      </a>
-      <a href="https://www.frontendmentor.io/profile/OluwadaraDaily" target="__blank">
-        <FrontEndMentor />
-      </a>
+    <div class="portfolio__header__links-and-image">
+      <div class="portfolio__header__links">
+        <a href="https://github.com/OluwadaraDaily" target="__blank">
+          <GitHub />
+        </a>
+        <a href="https://www.linkedin.com/in/oluwadara-oloye" target="__blank">
+          <LinkedIn />
+        </a>
+        <a href="https://twitter.com/OluwadaraDaily" target="__blank">
+          <Twitter />
+        </a>
+        <a href="https://www.frontendmentor.io/profile/OluwadaraDaily" target="__blank">
+          <FrontEndMentor />
+        </a>
+      </div>
     </div>
   </header>
 </template>
