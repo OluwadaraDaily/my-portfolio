a87a764a2a4e2b6e5fd0fa8cdeae87529e6c7e34
Oluwadara Oloye <daraoloye99@gmail.com>
2023-05-05 20:11:52 +0100

    Complete header component; fully responsive

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -3,17 +3,23 @@ import GitHub from '../icons/GitHub.vue'
 import LinkedIn from '../icons/LinkedIn.vue'
 import Twitter from '../icons/Twitter.vue'
 import FrontEndMentor from '../icons/FrontEndMentor.vue'
+import { RouterLink } from 'vue-router'
 </script>
 
 <template>
   <header>
-    <div class="portfolio__header__name">
-      <p class="portfolio__header__name__title">oluwadara oloye</p>
-    </div>
-    <div class="portfolio__header__links-and-image">
+    <div class="main-header">
+      <div class="portfolio__header__name">
+        <RouterLink to="/">
+          <p class="portfolio__header__name__title">oluwadara oloye</p>
+        </RouterLink>
+      </div>
       <div class="portfolio__header__links">
         <a href="https://github.com/OluwadaraDaily" target="__blank">
           <GitHub />
+        </a>
+        <a href="https://www.frontendmentor.io/profile/OluwadaraDaily" target="__blank">
+          <FrontEndMentor />
         </a>
         <a href="https://www.linkedin.com/in/oluwadara-oloye" target="__blank">
           <LinkedIn />
@@ -21,10 +27,10 @@ import FrontEndMentor from '../icons/FrontEndMentor.vue'
         <a href="https://twitter.com/OluwadaraDaily" target="__blank">
           <Twitter />
         </a>
-        <a href="https://www.frontendmentor.io/profile/OluwadaraDaily" target="__blank">
-          <FrontEndMentor />
-        </a>
       </div>
+    </div>
+    <div class="portfolio__header__image">
+      <img src="../../assets/images/oluwadara.png" class="portfolio__header__image_img" alt="">
     </div>
   </header>
 </template>
